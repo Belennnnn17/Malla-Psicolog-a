836e0f767fee201ff305252cc8840632169d6c77
Belennnnn17 <belenrojas1712@gmail.com>
2025-07-20 00:12:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ramos = document.querySelectorAll(".ramo");
+
+  const ramoMap = {};
+  ramos.forEach(ramo => {
+    ramoMap[ramo.id] = ramo;
+    ramo.disabled = ramo.dataset.prereqs !== "";
+  });
+
+  ramos.forEach(ramo => {
+    ramo.addEventListener("click", () => {
+      if (!ramo.disabled) {
+        ramo.classList.toggle("aprobado");
+
+        ramos.forEach(other => {
+          if (other.dataset.prereqs) {
+            const prereqs = other.dataset.prereqs.split(",");
+            const allMet = prereqs.every(id => ramoMap[id].classList.contains("aprobado"));
+            other.disabled = !allMet;
+          }
+        });
+      }
+    });
+  });
+});
 
